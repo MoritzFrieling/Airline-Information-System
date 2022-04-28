ce3de6d11601940615fb33a53ba52b7ee8de30d7
AHavertz <70887163+AHavertz@users.noreply.github.com>
2022-04-28 10:03:49 +0200

    not finished
    .

--- implementation/DataRecords/src/main/java/datarecords/FlightData.java
+++ implementation/DataRecords/src/main/java/datarecords/FlightData.java
@@ -10,13 +10,31 @@ public class FlightData {
     private final PlaneData plane;
     private final RouteData route;
     private final float price;
+    private final int economy;
+    private final int business;
+    private final int firstClass;
 
-    public FlightData(RouteData route, LocalDateTime departure, LocalDateTime arrival, PlaneData plane, float price) {
+    public FlightData(RouteData route, LocalDateTime departure, LocalDateTime arrival, PlaneData plane, float price, int economy, int business, int firstClass) {
         this.route = route;
         this.departure = departure;
         this.arrival = arrival;
         this.plane = plane;
         this.price = price;
+        this.business = business;
+        this.economy = economy;
+        this.firstClass = firstClass;
+    }
+
+    public int getEconomy() {
+        return economy;
+    }
+
+    public int getBusiness() {
+        return business;
+    }
+
+    public int getFirstClass() {
+        return firstClass;
     }
 
     public RouteData getRoute() {
--- implementation/GUI/src/main/java/gui/FlightController.java
+++ implementation/GUI/src/main/java/gui/FlightController.java
@@ -18,15 +18,13 @@ import java.util.function.Supplier;
 
 class FlightController implements Initializable {
     @FXML
-    public ComboBox modelDropDownOrigin;
-    @FXML
-    public ComboBox modelDropDownDestination;
+    public ComboBox modelDropDownRoute;
     @FXML
     public DatePicker datePickerDeparture;
     @FXML
     public ComboBox modelDropDownPlane;
     @FXML
-    public TextField durationMinutes;
+    public TextField seatClasses;
     @FXML
     public TextField departureTime;
     @FXML
@@ -50,45 +48,57 @@ class FlightController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        modelDropDownOrigin.getItems().addAll("-- select origin --","Berlin");
-        modelDropDownDestination.getItems().addAll("-- select destination --","New York");
-        modelDropDownOrigin.getSelectionModel().select("-- select origin --");
-        modelDropDownDestination.getSelectionModel().select("-- select destination --");
+        //Drop down for the available routes
+        modelDropDownRoute.getItems().addAll("-- select route --","Berlin -> New York");
+        modelDropDownRoute.getSelectionModel().select("-- select origin --");
 
+        //Drop down for the available planes
         modelDropDownPlane.getItems().addAll("-- select plane --","ATR 42-300","ATR 72","Airbus A300");
-
-
-
     }
 
     @FXML
     private void storeFlight() {
-
         FlightData flightData = createFlight();
-
         flightManager.add(flightData);
-
         result.setText("Flight added:\n" + flightData.toString());
-
     }
 
     private FlightData createFlight() {
-        if (modelDropDownOrigin.getSelectionModel().isSelected(1) && modelDropDownDestination.getSelectionModel().isSelected(1) && modelDropDownPlane.getSelectionModel().isSelected(1) && !departureTime.getText().isEmpty() && !durationMinutes.getText().isEmpty()) {
+        if (modelDropDownRoute.getSelectionModel().isSelected(1) && modelDropDownPlane.getSelectionModel().isSelected(1) && !departureTime.getText().isEmpty()) {
 
+            //Origin and destination for the route
             AirportData origin = new AirportData( "Berlin Flughafen", "BRLN", "Germany", new CoordinateData(40.446,-79.982));
             AirportData destination = new AirportData( "New York Airport", "NY", "USA", new CoordinateData(30.456,-20.345));
+
+            //the route of the flight, distance is calculated with the help of the coordinates
             RouteData route = new RouteData(origin, destination,
-                    new Coordinate( origin.getCoordinateData() ).rangeTo( new Coordinate( destination.getCoordinateData() ) )
+                    new Coordinate( origin.getCoordinateData() ).rangeTo(new Coordinate( destination.getCoordinateData()))
             );
 
+            //plane for the flight
             PlaneData plane = new PlaneData("9",new PlaneModelData("Avions de Transport Regional","42-300",50,1150,4950));
-            String[] split = departureTime.getText().split(":");
 
-            LocalDateTime departureDateTime = datePickerDeparture.getValue().atTime(LocalTime.of(Integer.valueOf(split[0]),Integer.valueOf(split[1])));
-            LocalDateTime arrivalDateTime = departureDateTime.plusMinutes(Integer.valueOf(durationMinutes.getText()));
+            //splits the departure time into hours and minutes
+            String[] splitDepartureTime = departureTime.getText().split(":");
 
+            //EST. Departure time
+            LocalDateTime departureDateTime = datePickerDeparture.getValue().atTime(LocalTime.of(Integer.valueOf(splitDepartureTime[0]),Integer.valueOf(splitDepartureTime[1])));
+
+            //EST. Arrival time
+            int duration = 120;
+            LocalDateTime arrivalDateTime = departureDateTime.plusMinutes(duration);
+
+            //default flight price
             int price = Integer.valueOf(defaultFlightPrice.getText());
 
+            //splits the seats for each seatClass (economy, business, firstClass)
+
+
+            //number of seats for each seatClass
+
+
+            //returns the finished flight with:
+            //route, departure time, arrival time, plane, price and number of seats for each class(economy, business, firstClass)
             return new FlightData(route, departureDateTime, arrivalDateTime, plane, price);
         } else {
             result.setText("Please ensure picking the first entry for plane, origin and destination!\nThe format for departure time is 'hh:mm' -> '14:30'\nPlease fill out every field.");
