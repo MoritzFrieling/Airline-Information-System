b6f0cde4de49147110dcb96d193657bd3f6a5cf2
JannickG <70884188+JannickG@users.noreply.github.com>
2022-05-23 13:57:48 +0200

    update comments

--- implementation/Persistence/src/main/java/persistence/StorageService.java
+++ implementation/Persistence/src/main/java/persistence/StorageService.java
@@ -7,16 +7,7 @@ import java.util.List;
 
 public interface StorageService {
 
-//    AirportData add(AirportData airportData) throws Exception;
-//    PlaneData add(PlaneData planeData) throws Exception;
-//    RouteData add(RouteData routeData);
-//    FlightData add(FlightData flightData);
-//    boolean add(PlaneModelData planeModelData) throws Exception;
-
     <T> boolean add(T t);
-
-   // boolean add(AccountData accountData) throws Exception;
-
 
     PasswordAuthentication getAccountData(PasswordAuthentication passwordAuthentication) throws Exception;
     String getPosition(String str);
