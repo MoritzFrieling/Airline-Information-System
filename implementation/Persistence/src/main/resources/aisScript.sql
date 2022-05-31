fead315a584aacd6860631edf51cd84603959689
JannickG <70884188+JannickG@users.noreply.github.com>
2022-05-31 21:57:09 +0200

    Timezones in Database
    Database table with timezones based on the longitude

--- implementation/Persistence/src/main/resources/aisScript.sql
+++ implementation/Persistence/src/main/resources/aisScript.sql
@@ -182,57 +182,57 @@ create table aisdb.ais.timezones(
     "abbreviation" varchar primary key,
     "name" string,
     "standard-deviation" int,
-    "startLat" float(6) not null,
-    "endLat" float(6) not null
+    "startLong" float(6) not null,
+    "endLong" float(6) not null
 );
 ---------------------------------------------------------------------
-INSERT INTO aisdb.ais.timezones(abbreviation, "name", "standard-deviation", "startLat", "endLat")
+INSERT INTO aisdb.ais.timezones(abbreviation, "name", "standard-deviation", "startLong", "endLong")
 VALUES ('UTC', 'Universal Coordinated Time', 0, 0, 14.99999);
-INSERT INTO aisdb.ais.timezones(abbreviation, "name", "standard-deviation", "startLat", "endLat")
+INSERT INTO aisdb.ais.timezones(abbreviation, "name", "standard-deviation", "startLong", "endLong")
 VALUES ('ECT', 'European Central Time', 1, 15, 29.99999);
-INSERT INTO aisdb.ais.timezones(abbreviation, "name", "standard-deviation", "startLat", "endLat")
+INSERT INTO aisdb.ais.timezones(abbreviation, "name", "standard-deviation", "startLong", "endLong")
 VALUES ('EET', 'Eastern European Time', 2, 30, 44.99999);
-INSERT INTO aisdb.ais.timezones(abbreviation, "name", "standard-deviation", "startLat", "endLat")
+INSERT INTO aisdb.ais.timezones(abbreviation, "name", "standard-deviation", "startLong", "endLong")
 VALUES ('EAT', 'Eastern African Time', 3, 45, 59.99999);
-INSERT INTO aisdb.ais.timezones(abbreviation, "name", "standard-deviation", "startLat", "endLat")
+INSERT INTO aisdb.ais.timezones(abbreviation, "name", "standard-deviation", "startLong", "endLong")
 VALUES ('NET', 'Near East Time', 4, 60, 74.99999);
-INSERT INTO aisdb.ais.timezones(abbreviation, "name", "standard-deviation", "startLat", "endLat")
+INSERT INTO aisdb.ais.timezones(abbreviation, "name", "standard-deviation", "startLong", "endLong")
 VALUES ('PLT', 'Pakistan Lahore Time', 5, 75, 89.99999);
-INSERT INTO aisdb.ais.timezones(abbreviation, "name", "standard-deviation", "startLat", "endLat")
+INSERT INTO aisdb.ais.timezones(abbreviation, "name", "standard-deviation", "startLong", "endLong")
 VALUES ('BST', 'Bangladesh Standard Time', 6, 90, 104.99999);
-INSERT INTO aisdb.ais.timezones(abbreviation, "name", "standard-deviation", "startLat", "endLat")
+INSERT INTO aisdb.ais.timezones(abbreviation, "name", "standard-deviation", "startLong", "endLong")
 VALUES ('VST', '   Vietnam Standard Time', 7, 105, 119.99999);
-INSERT INTO aisdb.ais.timezones(abbreviation, "name", "standard-deviation", "startLat", "endLat")
+INSERT INTO aisdb.ais.timezones(abbreviation, "name", "standard-deviation", "startLong", "endLong")
 VALUES ('CTT', 'China Taiwan Time', 8, 120, 134.99999);
-INSERT INTO aisdb.ais.timezones(abbreviation, "name", "standard-deviation", "startLat", "endLat")
+INSERT INTO aisdb.ais.timezones(abbreviation, "name", "standard-deviation", "startLong", "endLong")
 VALUES ('JST', 'Japan Standard Time', 9, 135, 149.99999);
-INSERT INTO aisdb.ais.timezones(abbreviation, "name", "standard-deviation", "startLat", "endLat")
+INSERT INTO aisdb.ais.timezones(abbreviation, "name", "standard-deviation", "startLong", "endLong")
 VALUES ('AET', 'Australia Eastern Time', 10, 150, 164.99999);
-INSERT INTO aisdb.ais.timezones(abbreviation, "name", "standard-deviation", "startLat", "endLat")
+INSERT INTO aisdb.ais.timezones(abbreviation, "name", "standard-deviation", "startLong", "endLong")
 VALUES ('SST', 'Solomon Standard Time', 11, 165, 179.99999);
-INSERT INTO aisdb.ais.timezones(abbreviation, "name", "standard-deviation", "startLat", "endLat")
+INSERT INTO aisdb.ais.timezones(abbreviation, "name", "standard-deviation", "startLong", "endLong")
 VALUES ('NST', 'New Zealand Standard Time', 12, 180, -165.00001);
-INSERT INTO aisdb.ais.timezones(abbreviation, "name", "standard-deviation", "startLat", "endLat")
+INSERT INTO aisdb.ais.timezones(abbreviation, "name", "standard-deviation", "startLong", "endLong")
 VALUES ('CAT', 'Central African Time', -1, -0.00001, -15);
-INSERT INTO aisdb.ais.timezones(abbreviation, "name", "standard-deviation", "startLat", "endLat")
+INSERT INTO aisdb.ais.timezones(abbreviation, "name", "standard-deviation", "startLong", "endLong")
 VALUES ('GST', 'Greenland Standard Time', -2, -15.00001, -30);
-INSERT INTO aisdb.ais.timezones(abbreviation, "name", "standard-deviation", "startLat", "endLat")
+INSERT INTO aisdb.ais.timezones(abbreviation, "name", "standard-deviation", "startLong", "endLong")
 VALUES ('BET', 'Brazil Eastern Time', -3, -30.00001, -45);
-INSERT INTO aisdb.ais.timezones(abbreviation, "name", "standard-deviation", "startLat", "endLat")
+INSERT INTO aisdb.ais.timezones(abbreviation, "name", "standard-deviation", "startLong", "endLong")
 VALUES ('PRT', '   Puerto Rico and US Virgin Islands Time', -4, -45.00001, -60);
-INSERT INTO aisdb.ais.timezones(abbreviation, "name", "standard-deviation", "startLat", "endLat")
+INSERT INTO aisdb.ais.timezones(abbreviation, "name", "standard-deviation", "startLong", "endLong")
 VALUES ('EST', '   Eastern Standard Time', -5, -60.00001, -75);
-INSERT INTO aisdb.ais.timezones(abbreviation, "name", "standard-deviation", "startLat", "endLat")
+INSERT INTO aisdb.ais.timezones(abbreviation, "name", "standard-deviation", "startLong", "endLong")
 VALUES ('CST', 'Central Standard Time', -6, -75.00001, -90);
-INSERT INTO aisdb.ais.timezones(abbreviation, "name", "standard-deviation", "startLat", "endLat")
+INSERT INTO aisdb.ais.timezones(abbreviation, "name", "standard-deviation", "startLong", "endLong")
 VALUES ('MST', 'Mountain Standard Time', -7, -90.00001, -105);
-INSERT INTO aisdb.ais.timezones(abbreviation, "name", "standard-deviation", "startLat", "endLat")
+INSERT INTO aisdb.ais.timezones(abbreviation, "name", "standard-deviation", "startLong", "endLong")
 VALUES ('PST', 'Pacific Standard Time', -8, -105.000001, -120);
-INSERT INTO aisdb.ais.timezones(abbreviation, "name", "standard-deviation", "startLat", "endLat")
+INSERT INTO aisdb.ais.timezones(abbreviation, "name", "standard-deviation", "startLong", "endLong")
 VALUES ('AST', '   Alaska Standard Time', -9, -120.00001, -135);
-INSERT INTO aisdb.ais.timezones(abbreviation, "name", "standard-deviation", "startLat", "endLat")
+INSERT INTO aisdb.ais.timezones(abbreviation, "name", "standard-deviation", "startLong", "endLong")
 VALUES ('HST', 'Hawaii Standard Time', -10, -135.00001, -150);
-INSERT INTO aisdb.ais.timezones(abbreviation, "name", "standard-deviation", "startLat", "endLat")
+INSERT INTO aisdb.ais.timezones(abbreviation, "name", "standard-deviation", "startLong", "endLong")
 VALUES ('MIT', 'Midway Islands Time', -11, -150.00001, -165);
 
 ---------------------------------------------------------------------
